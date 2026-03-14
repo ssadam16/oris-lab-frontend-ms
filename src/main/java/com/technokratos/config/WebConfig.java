0c81e802f1368b5d19cd87f2c6e0f55fe819b88d
”ssadam16” <ssdmllv@outlook.com>
2026-03-14 11:01:46 +0300

    user components are addeed

--- src/main/java/com/technokratos/config/WebConfig.java
+++ src/main/java/com/technokratos/config/WebConfig.java
@@ -1,11 +1,18 @@
 package com.technokratos.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.technokratos")
 public class WebConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
---
+++ src/main/java/com/technokratos/dto/UserResponse.java
@@ -0,0 +1,9 @@
+package com.technokratos.dto;
+
+import lombok.Builder;
+
+@Builder
+public record UserResponse(
+
+) {
+}
