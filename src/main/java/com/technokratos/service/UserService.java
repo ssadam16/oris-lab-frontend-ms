efe74850290464d9efa58088f05facb8f6330f2f
nongrataqqq <bogdan-747@mail.ru>
2026-03-14 11:52:28 +0300

    technical commit

--- src/main/java/com/technokratos/service/UserService.java
+++ src/main/java/com/technokratos/service/UserService.java
@@ -1,9 +1,11 @@
 package com.technokratos.service;
 
+import com.technokratos.config.MicroservicesProperties;
 import com.technokratos.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.UUID;
 
@@ -12,7 +14,10 @@ import java.util.UUID;
 @Slf4j
 public class UserService {
 
-    public UserResponse getUserById(UUID id) {
+    private final RestTemplate restTemplate;
+private final MicroservicesProperties microservicesProperties;
 
+    public UserResponse getUserById(UUID id) {
+        return null;
     }
 }
